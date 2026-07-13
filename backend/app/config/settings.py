003b33dfb1a45a2dbd75405f6397f75fe5b25f98
Shivani200820 <dahiphaleshivani8@gmail.com>
2026-07-13 21:45:48 +0530

    feat: add application configuration and FastAPI initialization

---
+++ backend/app/config/settings.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    APP_VERSION: str
+    DEBUG: bool
+
+    HOST: str
+    PORT: int
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    DATABASE_URL: str
+
+    CLOUDINARY_CLOUD_NAME: str = ""
+    CLOUDINARY_API_KEY: str = ""
+    CLOUDINARY_API_SECRET: str = ""
+
+    GROQ_API_KEY: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True
+    )
+
+
+settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.config.settings import settings
+from app.api.routers import health
 
-@app.get("/")
+# Create FastAPI application
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    description="AI Powered Smart Civic Complaint & Resolution Platform",
+)
+
+# Root Endpoint
+@app.get("/", tags=["Root"])
 def root():
-    return {"message": "Backend is running"}
+    return {
+        "message": "Welcome to CivicAI Backend"
+    }
+
+# Include Routers
+app.include_router(health.router)
+
